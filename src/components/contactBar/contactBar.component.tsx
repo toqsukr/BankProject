6e01e8e6cef3efa64cb19615fccc8ee316e9db56
Wastard <Ilya.pakylov@gmail.com>
2023-10-11 00:25:32 +0300

    added add contact button

--- src/components/contactBar/contactBar.component.tsx
+++ src/components/contactBar/contactBar.component.tsx
@@ -1,5 +1,6 @@
 import BarLayout from '@components/layouts/barLayout/barLayout.component';
 import SectionHeader from '@components/sectionHeader/sectionHeader.component';
+import PlusIcon from '@components/ui/icons/plusIcon.component';
 import { contacts } from './contactBar.data';
 import css from './contactBar.module.css';
 import ContactElement from './contactElement/contactElement.component';
@@ -9,6 +10,12 @@ const ContactBar = () => {
     <BarLayout>
       <SectionHeader title='Recent Contacts' otherText='All Contacts' />
       <div id={css.contactContainer}>
+        <div id={css.addContact}>
+          <div id={css.addContactCircle}>
+            <PlusIcon />
+          </div>
+          <span>Add</span>
+        </div>
         {contacts.map((contact, index) => (
           <ContactElement key={index} {...contact} />
         ))}
