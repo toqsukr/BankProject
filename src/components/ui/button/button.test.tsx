e3d9a15eeeb8eb86e2e82b8ac3ebb70f3d6e2063
Wastard <Ilya.pakylov@gmail.com>
2023-12-03 00:31:36 +0300

    all tests passed

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import Button from './button.component';
-import PlusIcon from '../icons/plusIcon.component';
 
 describe('Button tests', () => {
   it('Empty button render', () => {
@@ -11,14 +10,10 @@ describe('Button tests', () => {
   });
 
   it('Not empty button render', () => {
-    render(<Button type='button' text='кнопка' color='purple' icon={PlusIcon} />);
+    render(<Button text='кнопка' />);
 
     const button = screen.getByRole<HTMLButtonElement>('button');
     expect(button).toBeTruthy();
-
-    expect(button).toHaveAttribute('color', 'purple');
-    expect(button).toHaveAttribute('type', 'button');
-    expect(button).toHaveAttribute('icon', PlusIcon);
     expect(button.textContent).toBe('кнопка');
   });
 });
